2fb2ba65119566f2f9585df4d01223c41f283ec5
Erick Lucas <99370640+ErickElc@users.noreply.github.com>
2022-12-23 09:00:37 -0300

    Initial Commit

---
+++ eggplat-with storyboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  eggplat-with storyboard
+//
+//  Created by Erick Lucas Costa do Nascimento on 23/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
